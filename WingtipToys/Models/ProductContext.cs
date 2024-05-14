1a7a33f2cfea9a675a1c300eb517e637a5e1b79e
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-05-14 20:58:58 -0300

    added addToCart and ShoppingCart

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -15,5 +15,6 @@ namespace WingtipToys.Models
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
