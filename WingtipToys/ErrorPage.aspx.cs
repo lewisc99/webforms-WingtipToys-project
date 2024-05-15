56f0c795770b32fa33c8e1813ad0ddbafb09c27b
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-05-15 18:24:51 -0300

    added error page

---
+++ WingtipToys/ErrorPage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WingtipToys.Logic;
+
+namespace WingtipToys
+{
+    public partial class ErrorPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
